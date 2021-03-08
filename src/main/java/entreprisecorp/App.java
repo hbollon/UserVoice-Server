8f5a4fc8c4ec9459e6eb757fce391249e575d24b
Hugo <hugo.bollon@gmail.com>
2021-03-08 15:25:48 +0100

    Install SpringBoot and init app server

--- src/main/java/entreprisecorp/App.java
+++ src/main/java/entreprisecorp/App.java
@@ -1,9 +1,11 @@
 package entreprisecorp;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
