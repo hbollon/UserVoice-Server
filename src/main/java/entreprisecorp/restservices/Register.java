03c90f5f6270aa9d5508fe9b136f8a281a78e49b
Hugo <hugo.bollon@gmail.com>
2021-03-08 17:06:17 +0100

    Add .env.example

---
+++ src/main/java/entreprisecorp/restservices/Register.java
@@ -0,0 +1,26 @@
+package entreprisecorp.restservices;
+
+public class Register {
+    private final long id;
+    private final String content;
+    private final boolean success;
+
+    public Register(long id, String content, boolean success) {
+        this.id = id;
+        this.content = content;
+        this.success = success;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public boolean getSucess() {
+        return success;
+    }
+}
+
